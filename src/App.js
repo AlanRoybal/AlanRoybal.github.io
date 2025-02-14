280b90fd7c6fb523de9458576eeed9ce56950d7a
Alan Roybal <alanroybal05@gmail.com>
2025-02-14 00:46:45 -0600

    update hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
